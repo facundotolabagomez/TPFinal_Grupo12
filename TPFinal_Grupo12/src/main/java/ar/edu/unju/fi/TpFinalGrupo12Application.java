67240e00bb5adb30d3eb672a6c156a807f26660c
Facu TG <Facu TG@DESKTOP-SAMSUNG>
2022-06-15 16:16:55 -0300

    Primer Commit

---
+++ TPFinal_Grupo12/src/main/java/ar/edu/unju/fi/TpFinalGrupo12Application.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpFinalGrupo12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpFinalGrupo12Application.class, args);
+	}
+
+}
---
+++ TPFinal_Grupo12/src/test/java/ar/edu/unju/fi/TpFinalGrupo12ApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpFinalGrupo12ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
